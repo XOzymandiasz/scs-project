7a5856ffea302c66a549201c2833496a1e2f10a9
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-03-18 19:07:16 +0100

    feat: ttp

---
+++ cmd/ttp/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"scs-project/internal/protocol"
+	"scs-project/internal/transport"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(listener)
+
+	fmt.Println("Third part listening on :8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}(conn)
+
+	data, err := transport.Receive(conn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	msg, _ := protocol.Decode(data)
+	fmt.Println("Received:", msg.Type)
+
+	if msg.Type == "Ping" {
+		response := protocol.Message{
+			Type: "Pong",
+			Body: "Cert",
+		}
+
+		encoded, _ := protocol.Encode(response)
+		err := transport.Send(conn, encoded)
+		if err != nil {
+			return
+		}
+	}
+}
